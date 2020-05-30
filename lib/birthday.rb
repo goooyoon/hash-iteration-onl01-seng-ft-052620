82b4396e6d65c2fbb881e0b73fa5c590cbb024a6
Grace Kim <goooyoon@gmail.com>
2020-05-30 21:36:22 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,3 +10,12 @@ def happy_birthday(birthday_kids)
   end
 end
 
+
+
+## BONUS ##
+## Change "xit" to "it" in the test block to enable	## Change "xit" to "it" in the test block to enable
+describe "#age_appropriate_birthday" do	describe "#age_appropriate_birthday" do
+  xit "only prints the birthday greeting if the birthday kid 12 or younger" do	  it "only prints the birthday greeting if the birthday kid 12 or younger" do
+    birthday_kids = {	    birthday_kids = {
+      "Timmy" => 9,	      "Timmy" => 9,
+      "Sarah" => 6,	      "Sarah" => 6,
